361154400a8c03ff7258be35c39270344651c143
Matt Farmer <matt@frmr.me>
2014-07-28 18:04:52 -0400

    Update vain-test for latest version of vain.

--- app.js
+++ app.js
@@ -4,26 +4,21 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 var errorhandler = require("errorhandler");
 var vain = require("vain");
 
-// view engine setup
+app.set('view cache', false);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('vain').__express);
 
-app.use(favicon());
+
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.engine('html', vain.renderFile);
+app.use('/', vain.router(app.get('views')));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,8 +53,9 @@ app.use(function(err, req, res, next) {
 });
 
 // Snippets
-vain.registerSnippet("page-title", function($, element) {
-  $(element).text("Welcome to vain");
+vain.registerSnippet("page-title", function($, params, finished) {
+  $(this).text("Welcome to vain");
+  finished();
 });
 
 
